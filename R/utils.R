b6ede284e4fdcd7a54869b639f5871df7c8feb69
JooYoung Seo <sjysky@gmail.com>
2020-05-16 18:54:34 -0400

    Linting source code following tidyverse style.

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,13 @@
-find_resource <- function(template, file = 'template.tex') {
+find_resource <- function(template, file = "template.tex") {
   res <- system.file(
-    "rmarkdown", "templates", template, "resources", file, package = "edmdown"
+    "rmarkdown", "templates", template, "resources", file,
+    package = "edmdown"
   )
-  if (res == "") stop(
-    "Couldn't find template file ", template, "/resources/", file, call. = FALSE
-  )
+  if (res == "") {
+    stop(
+      "Couldn't find template file ", template, "/resources/", file,
+      call. = FALSE
+    )
+  }
   res
 }
